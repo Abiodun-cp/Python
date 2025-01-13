c0d484e134a203cb5b4afac1d8a3b633714c23ea
Abiodun-cp <aoluyinka@byupathway.edu>
2025-01-13 11:37:58 +0100

    code

---
+++ .py
@@ -0,0 +1,15 @@
+# Please enter the following:
+
+adjective = input("fluffy: ")
+animal = input("panda: ")
+verb1 = input("dancing ")
+verb2 = input("clap: ")
+verb3 = input("sing: ")
+exclamation = input("wow: ")
+
+# Your story is:
+print(f"The other day, I was really in trouble. It all started when I saw a very {adjective} { animal} {verb1} down the hallway.")
+print(f'"{exclamation.capitalize()}!" I yelled. But all I could think to do was to {verb2} over and over.')
+print(f"Miraculously, that caused it to stop, but not before it tried to {verb3} right in front of my family.")
+
+
