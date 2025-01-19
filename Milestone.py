2a6644867fd5907c90b1722749b442d0927d7127
Abiodun-cp <aoluyinka@byupathway.edu>
2025-01-19 21:19:45 +0100

    int

--- Milestone.py
+++ Milestone.py
@@ -1,29 +1,31 @@
 #prices of drinks and snacks and also quantity are added.
 
-#What is the price of a child's meal? $5.50
-#What is the price of an adult's meal? $7.00
-#How many children are there? 3
-#How many adults are there? 2
+#What is the price of a child's meal? $5.50 (float)
+#What is the price of an adult's meal? $7.00 (float)
+#How many children are there? 3 (int)
+#How many adults are there? 2 (int)
 
-#Subtotal: $30.5
+#Subtotal: $30.5 (float)
 
-#What is the sales tax rate? 5
-#Sales Tax: $1.525
-#Total: $32.025
+#What is the sales tax rate? 5 (int)
+#Sales Tax: $1.525 (float)
+#Total: $32.025 (float)
 
-#What is the payment amount? $45
-#change: $12.975
+#What is the payment amount? $45 (int)
+#change: $12.975 9float)
 
-#What is price of a drink? $2.00
-#How many drinks? 10
-#What is the price of a snack ? $6.00
-#How many snacks ? 10
+#What is price of a drink? $2.00(float)
+#How many drinks? 10 (int)
+#What is the price of a snack ? $6.00 (float)
+#How many snacks ? 10 (int)
 
-#Subtotal: $80.00
+#Subtotal: $80.00 (float)
 
-#What is the sales tax rate ? 20
-#Sales Tax: $16.00
-#Total: $96.00
+#What is the sales tax rate ? 20 (int)
+#Sales Tax: $16.00 (float)
+#Total: $96.00 (float)
 
-#What is the payment amount? $100.00
-#change:$4.00
+#What is the payment amount? $100.00(float)
+#change:$4.00 (float)
+
+
